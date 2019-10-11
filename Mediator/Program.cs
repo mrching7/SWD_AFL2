8e52bc3543dfb215d6e49b117e976f356d65a34e
zabih <44346668+mrching7@users.noreply.github.com>
2019-10-11 12:52:25 +0200

    startet

---
+++ Mediator/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Mediator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List <ConcreteCar> C = new List<ConcreteCar>();
+            List<ConcretePerson> P=new List<ConcretePerson>();
+            #region MyRegion
+            ConcreteCar C1=new ConcreteCar();
+            Thread.Sleep(20);
+            ConcreteCar C2 = new ConcreteCar();
+            Thread.Sleep(20);
+            ConcreteCar C3 = new ConcreteCar();
+            Thread.Sleep(20);
+            ConcreteCar C4 = new ConcreteCar();
+            Thread.Sleep(20);
+            ConcreteCar C5 = new ConcreteCar();
+            Thread.Sleep(20);
+            ConcreteCar C6 = new ConcreteCar();
+            Thread.Sleep(20);
+            ConcreteCar C7 = new ConcreteCar();
+            Thread.Sleep(20);
+            ConcreteCar C8 = new ConcreteCar();
+            Thread.Sleep(20);
+            ConcreteCar C9 = new ConcreteCar();
+            Thread.Sleep(20);
+            ConcreteCar C10 = new ConcreteCar();
+            Thread.Sleep(20);
+            ConcreteCar C11 = new ConcreteCar();
+            Thread.Sleep(20);
+            ConcreteCar C12 = new ConcreteCar();
+            Thread.Sleep(20);
+            ConcreteCar C13 = new ConcreteCar();
+            Thread.Sleep(20);
+            ConcreteCar C14 = new ConcreteCar();
+
+            ConcretePerson p1 = new ConcretePerson();
+            Thread.Sleep(20);
+            ConcretePerson p2 = new ConcretePerson();
+            Thread.Sleep(20);
+            ConcretePerson p3 = new ConcretePerson();
+            Thread.Sleep(20);
+            ConcretePerson p4 = new ConcretePerson();
+            Thread.Sleep(20);
+            ConcretePerson p5 = new ConcretePerson();
+            Thread.Sleep(20);
+            ConcretePerson p6 = new ConcretePerson();
+            Thread.Sleep(20);
+            ConcretePerson p7 = new ConcretePerson();
+            Thread.Sleep(20);
+            ConcretePerson p8 = new ConcretePerson();
+            Thread.Sleep(20);
+            ConcretePerson p9 = new ConcretePerson();
+            Thread.Sleep(20);
+            ConcretePerson p10 = new ConcretePerson();
+            Thread.Sleep(20);
+            ConcretePerson p11 = new ConcretePerson();
+            Thread.Sleep(20);
+            ConcretePerson p12 = new ConcretePerson();
+            Thread.Sleep(20);
+            ConcretePerson p13 = new ConcretePerson();
+            Thread.Sleep(20);
+            ConcretePerson p14 = new ConcretePerson();
+            Thread.Sleep(20);
+            #endregion
+
+            #region myregion2
+            C.Add(C1);
+            C.Add(C2);
+            C.Add(C3);
+            C.Add(C4);
+            C.Add(C5);
+            C.Add(C6);
+            C.Add(C7);
+            C.Add(C8);
+            C.Add(C9);
+            C.Add(C10);
+            C.Add(C11);
+            C.Add(C12);
+            C.Add(C13);
+            C.Add(C14);
+            P.Add(p1);
+            P.Add(p2);
+            P.Add(p3);
+            P.Add(p4);
+            P.Add(p5);
+            P.Add(p6);
+            P.Add(p7);
+            P.Add(p8);
+            P.Add(p9);
+            P.Add(p10);
+            P.Add(p11);
+            P.Add(p12);
+            P.Add(p13);
+            P.Add(p14);
+            #endregion
+
+            ConcreteTrafficLight concreteTraffic=new ConcreteTrafficLight(C, P);
+
+            concreteTraffic.NotifyForward();
+            Thread.Sleep(2000);
+            concreteTraffic.NotifyForward2();
+
+        }
+    }
+}
---
+++ Mediator/TrafficLight.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediator
+{
+    public interface TrafficLight
+    {
+        void NotifyForward();
+        void NotifyForward2();
+    }
+
+
+    public class ConcreteTrafficLight : TrafficLight
+    {
+        private List<Object> objects_;
+        public int queueN=0, QueueS=0, QueueW=0, QueueE=0;
+        private List<ConcreteCar> o_;
+        private List<ConcretePerson> p_;
+
+        public ConcreteTrafficLight(List<ConcreteCar> o, List<ConcretePerson> persons)
+        {
+            objects_=new List<Object>();
+            p_ = persons;
+            o_ = o;
+            objects_.AddRange(o_);
+            objects_.AddRange(p_);
+            
+
+            foreach (var p in objects_)
+            {
+                if (p.Location==0)
+                {
+                    queueN++;
+                }
+                else if (p.Location==1)
+                {
+                    QueueS++;
+                }
+                else if (p.Location == 2)
+                {
+                    QueueW++;
+                }
+                else if (p.Location == 3)
+                {
+                    QueueE++;
+                }
+            }
+        }
+
+        public void NotifyForward()
+        {
+            foreach (var p in objects_)
+            {
+                
+                if (p.Location == 0 && p.State == 0)
+                {
+                    p.CrossStreet();
+                    queueN--;
+                }
+                else if (p.Location == 1 && p.State == 0)
+                {
+                    p.CrossStreet();
+                    QueueS--;
+                }
+                else if (p.Location==0 && p.State==1)
+                {
+                    p.CrossStreet();
+                    queueN--;
+                }
+                else if (p.Location == 1 && p.State == 1)
+                {
+                    p.CrossStreet();
+                    QueueS--;
+                }
+                else if (p.Location == 0 && p.State == 2)
+                {
+                    if (QueueS > 0)
+                    {
+                        Console.WriteLine("Cant turn left from north waiting for opposite lane to clear");
+                    }
+                    else
+                    {
+                        p.CrossStreet();
+                        queueN--;
+                    }
+                }
+                else if (p.Location == 1 && p.State == 2)
+                {
+                    if (QueueS > 0)
+                    {
+                        Console.WriteLine("Cant turn left from south waiting for opposite lane to clear");
+                    }
+                    else
+                    {
+                        p.CrossStreet();
+                        QueueS--;
+                    }
+                }
+                
+            }
+        }
+
+
+        public void NotifyForward2()
+        {
+
+            foreach (var p in objects_)
+            {
+                if (p.Location == 2 && p.State == 0)
+                {
+                    p.CrossStreet();
+                    QueueW--;
+                }
+                else if (p.Location == 3 && p.State == 0)
+                {
+                    p.CrossStreet();
+                    QueueE--;
+                }
+                else if (p.Location == 2 && p.State == 1)
+                {
+                    p.CrossStreet();
+                    QueueW--;
+                }
+                else if (p.Location == 3 && p.State == 1)
+                {
+                    p.CrossStreet();
+                    QueueE--;
+                }
+                else if (p.Location == 2 && p.State == 2)
+                {
+                    if (QueueE > 0)
+                    {
+                        Console.WriteLine("Cant turn left from west waiting for opposite lane to clear");
+                    }
+                    else
+                    {
+                        p.CrossStreet();
+                        QueueW--;
+                    }
+                }
+                else if (p.Location == 3 && p.State == 2)
+                {
+                    if (QueueW > 0)
+                    {
+                        Console.WriteLine("Cant turn left from east waiting for opposite lane to clear");
+                    }
+                    else
+                    {
+                        p.CrossStreet();
+                        QueueE--;
+                    }
+                }
+
+            }
+
+
+
+
+        }
+    }
+
+}
