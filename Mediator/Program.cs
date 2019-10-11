283c00659780bf7abbe61c98b9be5ff0c76319c5
Kais <alikais98@gmail.com>
2019-10-11 13:47:36 +0200

    .

--- Mediator/Program.cs
+++ Mediator/Program.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,95 +20,27 @@ namespace Mediator
             List<ConcretePerson> PersonEast = new List<ConcretePerson>();
             List<ConcretePerson> PersonWest = new List<ConcretePerson>();
 
-            #region MyRegion
-            ConcreteCar C1=new ConcreteCar();
-            Thread.Sleep(20);
-            ConcreteCar C2 = new ConcreteCar();
-            Thread.Sleep(20);
-            ConcreteCar C3 = new ConcreteCar();
-            Thread.Sleep(20);
-            ConcreteCar C4 = new ConcreteCar();
-            Thread.Sleep(20);
-            ConcreteCar C5 = new ConcreteCar();
-            Thread.Sleep(20);
-            ConcreteCar C6 = new ConcreteCar();
-            Thread.Sleep(20);
-            ConcreteCar C7 = new ConcreteCar();
-            Thread.Sleep(20);
-            ConcreteCar C8 = new ConcreteCar();
-            Thread.Sleep(20);
-            ConcreteCar C9 = new ConcreteCar();
-            Thread.Sleep(20);
-            ConcreteCar C10 = new ConcreteCar();
-            Thread.Sleep(20);
-            ConcreteCar C11 = new ConcreteCar();
-            Thread.Sleep(20);
-            ConcreteCar C12 = new ConcreteCar();
-            Thread.Sleep(20);
-            ConcreteCar C13 = new ConcreteCar();
-            Thread.Sleep(20);
-            ConcreteCar C14 = new ConcreteCar();
 
-            ConcretePerson p1 = new ConcretePerson();
-            Thread.Sleep(20);
-            ConcretePerson p2 = new ConcretePerson();
-            Thread.Sleep(20);
-            ConcretePerson p3 = new ConcretePerson();
-            Thread.Sleep(20);
-            ConcretePerson p4 = new ConcretePerson();
-            Thread.Sleep(20);
-            ConcretePerson p5 = new ConcretePerson();
-            Thread.Sleep(20);
-            ConcretePerson p6 = new ConcretePerson();
-            Thread.Sleep(20);
-            ConcretePerson p7 = new ConcretePerson();
-            Thread.Sleep(20);
-            ConcretePerson p8 = new ConcretePerson();
-            Thread.Sleep(20);
-            ConcretePerson p9 = new ConcretePerson();
-            Thread.Sleep(20);
-            ConcretePerson p10 = new ConcretePerson();
-            Thread.Sleep(20);
-            ConcretePerson p11 = new ConcretePerson();
-            Thread.Sleep(20);
-            ConcretePerson p12 = new ConcretePerson();
-            Thread.Sleep(20);
-            ConcretePerson p13 = new ConcretePerson();
-            Thread.Sleep(20);
-            ConcretePerson p14 = new ConcretePerson();
-            Thread.Sleep(20);
-            #endregion
+            for (int i = 0; i <= 5; i++)
+            {
+                CarsNorth.Add(new ConcreteCar());
+                Thread.Sleep(20);
+                CarsSouth.Add(new ConcreteCar());
+                Thread.Sleep(20);
+                CarsEast.Add(new ConcreteCar());
+                Thread.Sleep(20);
+                CarsWest.Add(new ConcreteCar());
+                Thread.Sleep(20);
 
-            #region myregion2
-            C.Add(C1);
-            C.Add(C2);
-            C.Add(C3);
-            C.Add(C4);
-            C.Add(C5);
-            C.Add(C6);
-            C.Add(C7);
-            C.Add(C8);
-            C.Add(C9);
-            C.Add(C10);
-            C.Add(C11);
-            C.Add(C12);
-            C.Add(C13);
-            C.Add(C14);
-            P.Add(p1);
-            P.Add(p2);
-            P.Add(p3);
-            P.Add(p4);
-            P.Add(p5);
-            P.Add(p6);
-            P.Add(p7);
-            P.Add(p8);
-            P.Add(p9);
-            P.Add(p10);
-            P.Add(p11);
-            P.Add(p12);
-            P.Add(p13);
-            P.Add(p14);
-            #endregion
+                PersonNorth.Add(new ConcretePerson());
+                Thread.Sleep(20);
+                PersonSouth.Add(new ConcretePerson());
+                Thread.Sleep(20);
+                PersonEast.Add(new ConcretePerson());
+                Thread.Sleep(20);
+                PersonWest.Add(new ConcretePerson());
+                Thread.Sleep(20);
+            }
 
             ConcreteTrafficLight concreteTraffic=new ConcreteTrafficLight(CarsNorth, CarsSouth, CarsEast, CarsWest, PersonNorth, PersonSouth, PersonEast, PersonWest);
 
@@ -120,119 +51,3 @@ namespace Mediator
         }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Threading;
-
-namespace Mediator
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            List <ConcreteCar> C = new List<ConcreteCar>();
-            List<ConcretePerson> P=new List<ConcretePerson>();
-            #region MyRegion
-            ConcreteCar C1=new ConcreteCar();
-            Thread.Sleep(20);
-            ConcreteCar C2 = new ConcreteCar();
-            Thread.Sleep(20);
-            ConcreteCar C3 = new ConcreteCar();
-            Thread.Sleep(20);
-            ConcreteCar C4 = new ConcreteCar();
-            Thread.Sleep(20);
-            ConcreteCar C5 = new ConcreteCar();
-            Thread.Sleep(20);
-            ConcreteCar C6 = new ConcreteCar();
-            Thread.Sleep(20);
-            ConcreteCar C7 = new ConcreteCar();
-            Thread.Sleep(20);
-            ConcreteCar C8 = new ConcreteCar();
-            Thread.Sleep(20);
-            ConcreteCar C9 = new ConcreteCar();
-            Thread.Sleep(20);
-            ConcreteCar C10 = new ConcreteCar();
-            Thread.Sleep(20);
-            ConcreteCar C11 = new ConcreteCar();
-            Thread.Sleep(20);
-            ConcreteCar C12 = new ConcreteCar();
-            Thread.Sleep(20);
-            ConcreteCar C13 = new ConcreteCar();
-            Thread.Sleep(20);
-            ConcreteCar C14 = new ConcreteCar();
-
-            ConcretePerson p1 = new ConcretePerson();
-            Thread.Sleep(20);
-            ConcretePerson p2 = new ConcretePerson();
-            Thread.Sleep(20);
-            ConcretePerson p3 = new ConcretePerson();
-            Thread.Sleep(20);
-            ConcretePerson p4 = new ConcretePerson();
-            Thread.Sleep(20);
-            ConcretePerson p5 = new ConcretePerson();
-            Thread.Sleep(20);
-            ConcretePerson p6 = new ConcretePerson();
-            Thread.Sleep(20);
-            ConcretePerson p7 = new ConcretePerson();
-            Thread.Sleep(20);
-            ConcretePerson p8 = new ConcretePerson();
-            Thread.Sleep(20);
-            ConcretePerson p9 = new ConcretePerson();
-            Thread.Sleep(20);
-            ConcretePerson p10 = new ConcretePerson();
-            Thread.Sleep(20);
-            ConcretePerson p11 = new ConcretePerson();
-            Thread.Sleep(20);
-            ConcretePerson p12 = new ConcretePerson();
-            Thread.Sleep(20);
-            ConcretePerson p13 = new ConcretePerson();
-            Thread.Sleep(20);
-            ConcretePerson p14 = new ConcretePerson();
-            Thread.Sleep(20);
-            #endregion
-
-            #region myregion2
-            C.Add(C1);
-            C.Add(C2);
-            C.Add(C3);
-            C.Add(C4);
-            C.Add(C5);
-            C.Add(C6);
-            C.Add(C7);
-            C.Add(C8);
-            C.Add(C9);
-            C.Add(C10);
-            C.Add(C11);
-            C.Add(C12);
-            C.Add(C13);
-            C.Add(C14);
-            P.Add(p1);
-            P.Add(p2);
-            P.Add(p3);
-            P.Add(p4);
-            P.Add(p5);
-            P.Add(p6);
-            P.Add(p7);
-            P.Add(p8);
-            P.Add(p9);
-            P.Add(p10);
-            P.Add(p11);
-            P.Add(p12);
-            P.Add(p13);
-            P.Add(p14);
-            #endregion
-
-            ConcreteTrafficLight concreteTraffic=new ConcreteTrafficLight(C, P);
-
-            concreteTraffic.NotifyForward();
-            Thread.Sleep(2000);
-            concreteTraffic.NotifyForward2();
-
-        }
-    }
-}
->>>>>>> 5358bdb33a652a388d4af32adf2988ae0d3aa7a9
